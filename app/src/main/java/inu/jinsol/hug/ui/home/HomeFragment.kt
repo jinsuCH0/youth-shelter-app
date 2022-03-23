ab9b349e36b187be286fc93c8b8e36e8b1db3f19
Jin Su Cho <blacksky9795@naver.com>
2022-03-23 23:10:17 +0900

    03/23 위치접근 퍼미션 구현 완료

--- app/src/main/java/inu/jinsol/hug/ui/home/HomeFragment.kt
+++ app/src/main/java/inu/jinsol/hug/ui/home/HomeFragment.kt
@@ -3,6 +3,7 @@ package inu.jinsol.hug.ui.home
 import android.Manifest
 import android.app.Activity
 import android.app.AlertDialog
+import android.content.Context
 import android.content.Context.LOCATION_SERVICE
 import android.content.DialogInterface
 import android.content.Intent
@@ -18,7 +19,6 @@ import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import inu.jinsol.hug.databinding.FragmentHomeBinding
@@ -35,33 +35,45 @@ class HomeFragment : Fragment(), MapView.CurrentLocationEventListener {
         Manifest.permission.ACCESS_FINE_LOCATION,
         Manifest.permission.ACCESS_COARSE_LOCATION
     )
-    lateinit var locationActivityResultLauncher: ActivityResultLauncher<Intent>
+
+    private lateinit var thisContext: Context
+    private lateinit var locationActivityResultLauncher: ActivityResultLauncher<Intent>
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
 
-        val homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+        val homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java) // <- 필요한지 모르겠음
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
 
+        // fragment에 mapview 부착
         val mapView = MapView(activity)
         val mapViewContainer = binding.mapView
         mapViewContainer.addView(mapView)
 
+
         mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(37.53737528, 127.00557633), true);
+        mapView.setCurrentLocationEventListener(this) // <- 매개변수 오류
 
-        mapView.setCurrentLocationEventListener(mapView);
-        checkAllPermissions()
 
+        checkAllPermissions() // 퍼미션 체크
+
+        mapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOnWithHeading)
         return root
     }
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        thisContext = context
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
 
     private fun checkAllPermissions() {
         if (!isLocationServicesAvailable()) {
@@ -71,38 +83,25 @@ class HomeFragment : Fragment(), MapView.CurrentLocationEventListener {
         }
     }
 
-    fun isLocationServicesAvailable(): Boolean {
-        val locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
+    private fun isLocationServicesAvailable(): Boolean {
+        val locationManager = thisContext.getSystemService(LOCATION_SERVICE) as LocationManager // <- getSystemService(LOCATION_SERVICE) 오류
         return (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
                 || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER))
     }
 
-    fun isRunTimePermissionsGranted() {
-        val hasFineLocationPermission = ContextCompat.checkSelfPermission(
-            getActivity(),
-            Manifest.permission.ACCESS_FINE_LOCATION
-        )
-        val hasCoarseLocationPermission = ContextCompat.checkSelfPermission(
-            getActivity(),
-            Manifest.permission.ACCESS_COARSE_LOCATION
-        )
+    private fun isRunTimePermissionsGranted() {
+        val context = context ?: return
+        val hasFineLocationPermission = ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) // <- context 오류
+        val hasCoarseLocationPermission = ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) // <- context 오류
 
-        if (hasFineLocationPermission != PackageManager.PERMISSION_GRANTED ||
-            hasCoarseLocationPermission != PackageManager.PERMISSION_GRANTED
-        ) {
-            ActivityCompat.requestPermissions(
-                getActivity(),
-                REQUIRED_PERMISSIONS,
-                PERMISSIONS_REQUEST_CODE
-            )
+        if (hasFineLocationPermission != PackageManager.PERMISSION_GRANTED || hasCoarseLocationPermission != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(requireActivity(), REQUIRED_PERMISSIONS, PERMISSIONS_REQUEST_CODE) // <- context 오류
         }
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == PERMISSIONS_REQUEST_CODE && grantResults.size ==
-            REQUIRED_PERMISSIONS.size
-        ) {
+        if (requestCode == PERMISSIONS_REQUEST_CODE && grantResults.size == REQUIRED_PERMISSIONS.size) {
             var checkResult = true
 
             for (result in grantResults) {
@@ -112,9 +111,9 @@ class HomeFragment : Fragment(), MapView.CurrentLocationEventListener {
                 }
             }
             if (checkResult) {
-
+                // ?
             } else {
-                Toast.makeText(getActivity(), "퍼미션이 거부되었습니다. 앱을 다시 실행하여 퍼미션을 허용해주세요.", Toast.LENGTH_LONG).show()
+                Toast.makeText(activity, "퍼미션이 거부되었습니다. 앱을 다시 실행하여 퍼미션을 허용해주세요.", Toast.LENGTH_LONG).show()
                 //finish()  <- 액티비티 종료 메소드인데 프래그먼트 종료로 바꿔야 함
             }
         }
@@ -128,14 +127,14 @@ class HomeFragment : Fragment(), MapView.CurrentLocationEventListener {
                 if (isLocationServicesAvailable()) {
                     isRunTimePermissionsGranted()
                 } else {
-                    Toast.makeText(getActivity(), "위치 서비스를 사용할 수 없습니다.", Toast.LENGTH_LONG).show()
+                    Toast.makeText(activity, "위치 서비스를 사용할 수 없습니다.", Toast.LENGTH_LONG).show()
                     //finish()
                 }
             }
         }
 
         val builder: AlertDialog.Builder = AlertDialog.Builder(
-            getActivity()
+            activity
         )
         builder.setTitle("위치 서비스 비활성화")
         builder.setMessage(
@@ -145,21 +144,21 @@ class HomeFragment : Fragment(), MapView.CurrentLocationEventListener {
         builder.setCancelable(true)
         builder.setPositiveButton("설정",
             DialogInterface.OnClickListener { dialog, id ->
-                val callGPSSettingIntent = Intent(
-                    Settings.ACTION_LOCATION_SOURCE_SETTINGS
-                )
+                val callGPSSettingIntent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
                 locationActivityResultLauncher .launch(callGPSSettingIntent)
             })
         builder.setNegativeButton("취소",
             DialogInterface.OnClickListener { dialog, id -> dialog.cancel()
-                Toast.makeText(getActivity(), "기기에서 위치서비스(GPS) 설정 후 사용해주세요.", Toast.LENGTH_SHORT).show()
+                Toast.makeText(activity, "기기에서 위치서비스(GPS) 설정 후 사용해주세요.", Toast.LENGTH_SHORT).show()
                 //finish()
             })
         builder.create().show()
     }
 
+    // MapView.CurrentLocationEventListener 오버라이드
     override fun onCurrentLocationUpdate(p0: MapView?, p1: MapPoint?, p2: Float) {}
     override fun onCurrentLocationDeviceHeadingUpdate(p0: MapView?, p1: Float) {}
     override fun onCurrentLocationUpdateCancelled(p0: MapView?) {}
     override fun onCurrentLocationUpdateFailed(p0: MapView?) {}
+
 }
