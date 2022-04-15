03762407c58c5fb2fdf54ca3414485b4eec77562
Jin Su Cho <blacksky9795@naver.com>
2022-04-15 19:50:36 +0900

    쉼터찾기 레이아웃만 구현, 리스트 tv짤림 해결

--- app/src/main/java/inu/jinsol/hug/ui/sheltersearch/ShelterSearchFragment.kt
+++ app/src/main/java/inu/jinsol/hug/ui/sheltersearch/ShelterSearchFragment.kt
@@ -1,39 +1,61 @@
 package inu.jinsol.hug.ui.sheltersearch
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.SearchView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import inu.jinsol.hug.databinding.FragmentSheltersearchBinding
 
-class ShelterSearchFragment : Fragment() {
+class ShelterSearchFragment : Fragment(), SearchView.OnQueryTextListener {
 
     private var _binding: FragmentSheltersearchBinding? = null
     private val binding get() = _binding!!
 
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-
-        val sheltersearchViewModel = ViewModelProvider(this).get(ShelterSearchViewModel::class.java)
-
+        val sheltersearchViewModel = ViewModelProvider(this)[ShelterSearchViewModel::class.java]
         _binding = FragmentSheltersearchBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        val textView: TextView = binding.textSheltersearch
-        sheltersearchViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+//        val textView: TextView = binding.textSheltersearch
+//        sheltersearchViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
+
+        var searchView = binding.searchView
 
 
-        return root
+        Log.d(TAG, "너비: ${searchView.width}")
+        Log.d(TAG, "높이: ${searchView.height}")
+
+        searchView.setOnQueryTextListener(this)
+
+
+        return binding.root
     }
-
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onQueryTextSubmit(p0: String?): Boolean {
+        Log.d(TAG, "SearchView - onClick!: ")
+        Toast.makeText(context, "검색버튼 클릭!", Toast.LENGTH_SHORT).show()
+        var searchView = binding.searchView
+        Log.d(TAG, "너비: ${searchView.width}")
+        Log.d(TAG, "높이: ${searchView.height}")
+        return false
+    }
+
+    override fun onQueryTextChange(p0: String?): Boolean {
+        Log.d(TAG, "SearchView - onTyping- : ")
+        return false
     }
 
 }
